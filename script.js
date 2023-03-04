9786144edd21d64705903a8c079d340d288bb346
MishaFromZeroToHero <115973076+MishaFromZeroToHero@users.noreply.github.com>
2023-03-04 21:21:16 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const noBtn = document.querySelector(".no");
+const yesBtn = document.querySelector(".yes");
+const thxGif = document.querySelector(".alert-gif");
+const getRandomPosition = function (a, b) {
+  a = Math.ceil(a);
+  b = Math.floor(b);
+  return Math.floor(Math.random() * (b - a)) + a;
+};
+console.log(getRandomPosition(10, 20));
+
+noBtn.addEventListener("click", function () {
+  noBtn.style.position = "absolute";
+  noBtn.style.transform = `translate(${getRandomPosition(
+    -109,
+    109
+  )}px, ${getRandomPosition(-160, window.screen.height / 3)}px)`;
+});
+yesBtn.addEventListener("click", function () {
+  thxGif.style.display = "flex";
+  noBtn.style.display = "none";
+  yesBtn.style.display = "none";
+});
